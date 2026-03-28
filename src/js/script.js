6505ec2c62368cf0679597fd14c6e78a793e186c
Felipe Pedrol <164224648+fishpedrol@users.noreply.github.com>
2026-03-28 18:05:22 -0300

    feat: implementa fluxo de cadastro e compra de vinhos
    Adiciona função initsys responsável por:
    - Cadastro de usuário
    - Exibição de dados no console
    - Listagem de vinhos
    - Simulação de compra

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,38 @@
-alert('Bem Vindo ao Cadastro de Usuário!');
+function initsys() {
 
-let name = prompt('Digite seu nome:');
+    alert('Bem Vindo ao Cadastro de Usuário!');
 
-alert(`Olá, ${name}!`);
+    let name = prompt('Digite seu nome:');
 
+    alert(`Olá, ${name}!`);
+
+
+    let idade = prompt('Digite sua idade para o Cadastro:');
+    let mail = prompt('Digite seu Email para o Cadastro:');
+    let password = prompt('Digite sua senha para o Cadastro:');
+
+    alert('Cadastro realizado! Veja os detalhes no console.');
+
+    console.log(`Nome: ${name}  \n Idade: ${idade}  \n Email: ${mail}  \n Senha: XXXXXX`);
+
+    alert('A seguir, veja os detalhes do vinho no console.');
+
+    let vinhos = [
+        { nome: "Cantinho do Vale", tipos: "Tinto, Branco, Rosé", estoque: 10 },
+        { nome: "Salton", tipos: "Tinto, Branco, Rosé", estoque: 20 },
+        { nome: "Dom Nogueira", tipos: "Tinto, Branco, Rosé", estoque: 30 }
+    ];
+
+    console.log("🍷 Vinhos disponíveis:");
+    console.log('Nome: Cantinho do Vale, Salton, Dom Nogueira \n Tipo: Tinto, Branco, Rosé \n Safra: 2015, 2012, 2000 \n Estoque: 10, 20, 30');
+
+    let vinho = prompt('Digite o nome do vinho que deseja comprar:');
+    let type = prompt('Digite o tipo do vinho que deseja comprar:');
+    let safra =  prompt('Digite a safra do vinho que deseja comprar:');
+    let qtd = prompt('Digite a quantidade que deseja comprar:');
+
+    alert(`🧾 Escolha do cliente \n Vinho ${vinho}. \n Tipo ${type}. \n Safra ${safra}. \n Quantidade ${qtd}. \n Clique Enter para confirmar sua compra!`);
+
+    alert('✅ Compra confirmada! \n Obrigado por comprar conosco!');
+
+}
